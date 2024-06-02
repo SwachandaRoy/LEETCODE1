40dba5d2dfb519118840262a9c8b8336e9648916
SwachandaRoy <96686803+SwachandaRoy@users.noreply.github.com>
2024-06-02 11:01:42 +0530

    Update kadane_maxsubarray.py

--- kadane_maxsubarray.py
+++ kadane_maxsubarray.py
@@ -1,9 +1,12 @@
+#eg: [-2, 1, -3]
+
 def kadane(arr1):
-    max_curr=max_global=arr[0]
-    for ele in arr[1::]:
-        max_curr=max(ele, max_curr+ele)
-        max_global=max(max_curr, max_global)
-    return max_global
+    curr_sum=0
+    max_sum=arr[0]
+    for ele in arr1:
+        curr_sum=max(ele, max_curr+ele)
+        max_sum=max(max_sum, curr_sum)
+    return max_sum
 
 arr=list(map(int, input("Enter array:").split()))
 print("SOL: ",kadane(arr))
