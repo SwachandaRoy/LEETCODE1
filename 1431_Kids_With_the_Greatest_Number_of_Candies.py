b9d185f5af9f78a896e7d1d98731257fb2fdf1fa
SwachandaRoy <96686803+SwachandaRoy@users.noreply.github.com>
2024-06-19 09:26:30 +0530

    Create 1431_Kids_With_the_Greatest_Number_of_Candies.py

---
+++ 1431_Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        result=[]
+        for candy in candies:
+            result.append(candy+extraCandies>=max(candies))
+        return result
